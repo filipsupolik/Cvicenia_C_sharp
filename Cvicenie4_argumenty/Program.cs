01e07976d88c55513a1ea7ac43e8de0a9c67ad2b
filipsupolik <filipsupolik25@gmail.com>
2025-03-19 13:38:25 +0100

    Cvicenie 4 praca z nuget figgle kniznicou
    Cvicenie 4 praca z nuget figgle kniznicou

---
+++ Cvicenie4_argumenty/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
+
+Array.Sort(args);
+
+StringBuilder sb = new StringBuilder();
+
+int counter = 0;
+foreach (var argumnets in args)
+{
+    counter++;
+    Console.Write($"{argumnets.Length} ");
+    Console.WriteLine(argumnets);
+    sb.Append(argumnets);
+
+    
+}
+Console.WriteLine(sb);
